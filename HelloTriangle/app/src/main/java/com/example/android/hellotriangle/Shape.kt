7ad767d1f806f7a756d960a4c73a9638d4fbd70f
Denis Konoplev <dekonoplyov@gmail.com>
2018-07-21 20:22:40 +0300

    Two triangles with different vao/vbo

--- HelloTriangle/app/src/main/java/com/example/android/hellotriangle/Shape.kt
+++ HelloTriangle/app/src/main/java/com/example/android/hellotriangle/Shape.kt
@@ -2,19 +2,30 @@ package com.example.android.hellotriangle
 
 import android.opengl.GLES20
 import android.opengl.GLES30.*
+import java.nio.Buffer
+import java.nio.FloatBuffer
 
 class Shape {
     val vertices = floatArrayOf(
             -0.5f, -0.5f, 0f,
              0.5f, -0.5f, 0f,
-             0.5f,  0.5f, 0f,
-            -0.5f,  0.5f, 0f
+             0.5f,  0.5f, 0f
+    )
+
+    val vert1 = floatArrayOf(
+            0.5f, 0.7f, 0f,
+            -0.5f, 0.7f, 0f,
+            -0.5f,  0f, 0f
     )
 
     val verticesBuffer = floatBuffer(vertices.size)
             .put(vertices).position(0)
 
-     var vaoId = -1
+    val verticesBuffer1 = floatBuffer(vert1.size)
+            .put(vert1).position(0)
+
+    var vaoId = -1
+    var vaoId1 = -1
 
     val vertexShader =
             "attribute vec4 a_Position;" +
@@ -35,9 +46,15 @@ class Shape {
     init {
         compileProgram()
 
-        beginVAO()
+        vaoId = beginVAO()
 
-        setVertexBuffer()
+        setVertexBuffer(vertices.size * 4, verticesBuffer)
+
+        endVAO()
+
+        vaoId1 = beginVAO()
+
+        setVertexBuffer(vert1.size * 4, verticesBuffer1)
 
         endVAO()
     }
@@ -59,39 +76,42 @@ class Shape {
         glLinkProgram(programId)
     }
 
-    fun beginVAO() {
+    fun beginVAO(): Int {
         val vaoBuffer = intBuffer(1)
         glGenVertexArrays(1, vaoBuffer)
-        vaoId = vaoBuffer.get(0)
-        glBindVertexArray(vaoId)
+        glBindVertexArray(vaoBuffer.get(0))
+        return vaoBuffer.get(0)
     }
 
-    fun setVertexBuffer() {
+    fun setVertexBuffer(size: Int, buffer: Buffer) {
         val vboBuffer = intBuffer(1)
         glGenBuffers(1, vboBuffer)
-        val vboId = vboBuffer.get(0)
 
-        glBindBuffer(GL_ARRAY_BUFFER, vboId)
-        glBufferData(GL_ARRAY_BUFFER, vertices.size * 4, verticesBuffer, GL_STATIC_DRAW)
+        glBindBuffer(GL_ARRAY_BUFFER, vboBuffer.get(0))
+        glBufferData(GL_ARRAY_BUFFER, size, buffer, GL_STATIC_DRAW)
 
         val positionHandle = glGetAttribLocation(programId, "a_Position")
         val vertexStride = 3 * 4
         glVertexAttribPointer(positionHandle, 3, GL_FLOAT, false, vertexStride, 0)
         glEnableVertexAttribArray(positionHandle)
-
     }
 
     fun endVAO() {
-        glBindVertexArray(vaoId)
+        glBindVertexArray(0)
         glBindBuffer(GL_ARRAY_BUFFER, 0)
     }
 
     fun draw() {
         glUseProgram(programId)
 
-        glBindVertexArray(vaoId)
         val colorHandle = glGetUniformLocation(programId, "u_Color")
+
         glUniform4f(colorHandle, 1f, 0f, 0f, 1f)
+        glBindVertexArray(vaoId)
+        glDrawArrays(GL_TRIANGLES, 0, 3)
+
+        glUniform4f(colorHandle, 1f, 0f, 1f, 1f)
+        glBindVertexArray(vaoId1)
         glDrawArrays(GL_TRIANGLES, 0, 3)
 
         glBindVertexArray(0)
